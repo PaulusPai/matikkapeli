a4f79877d162d802004f57781ee55bb67a55d254
pauluspäivärinta <n3papa00@students.oamk.fi>
2024-02-06 13:14:58 +0200

    viikkotehtävä valmis

---
+++ functions/function.js
@@ -0,0 +1,39 @@
+'use strict'
+
+let rand_num1 = 0
+let rand_num2 = 0
+let vaarin = 0
+let oikein = 0
+
+const getRandonIntNumberInRange = (min,max) => {
+    return Math.floor(Math.random() * max) + min;
+}
+
+const randomizeNumbers = () => {
+    rand_num1 = getRandonIntNumberInRange(1,10)
+    rand_num2 = getRandonIntNumberInRange(1,10)
+    document.querySelector('#num1').innerHTML = rand_num1
+    document.querySelector('#num2').innerHTML = rand_num2
+}
+
+addEventListener("DOMContentLoaded", () => {
+    randomizeNumbers()
+});
+
+document.querySelector('button').addEventListener('click',() => {
+    const answer = Number(document.querySelector('input').value)
+    const correctAnswer = rand_num1 + rand_num2
+    if (answer === correctAnswer) {
+        alert('Correct!')
+        oikein += 1
+        document.querySelector('#oikein').innerHTML = oikein    
+    }   else {
+        alert('Incorrect!')
+        vaarin += 1
+        document.querySelector('#väärin').innerHTML = vaarin
+    }
+
+    randomizeNumbers()
+    document.querySelector('input').value=''
+})
+
